81a3f3593ed55e456ee974fbdb9d7ec7c958cbe0
Nikita-staric <moses.nikitos@gmail.com>
2021-09-08 21:46:45 +0300

    first commit

---
+++ NewProject.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //екземпрял Класса
+            classLogic class__Logic = new classLogic();
+            checkingEvent checking__Event = new checkingEvent();
+            checkingEvent1 checking___Event = new checkingEvent1();
+            //Подписались на событие
+            class__Logic.delegaate += checking__Event.outputCompletedCompleted;
+            class__Logic.delegaate += checking___Event.outputCompletedCompleted1;
+            //Запустили счетчик
+            class__Logic.counter();
+
+        }
+    }
+   // public delegate void Cc();
+    class classLogic
+    {
+        public delegate void NewDelegate();
+        public event NewDelegate delegaate;
+      //  public delegate void Cc();
+      
+      
+        public  void counter()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == 71)
+                {
+
+                    Console.WriteLine("71");
+                    delegaate();
+                    Console.ReadLine();
+                }
+                else
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        Console.WriteLine("71");
+                        delegaate();
+                        int vv = 0;
+                        Console.WriteLine($"{j}", vv++);
+                    }
+
+
+
+
+
+                }
+            }
+
+        }
+       
+    }
+    class checkingEvent
+    {
+        public void outputCompletedCompleted()
+        {
+            Console.WriteLine("event output 71");
+        }
+    }
+    class checkingEvent1
+    {
+        public void outputCompletedCompleted1()
+        {
+            Console.WriteLine("there will definitely be a conclusion here");
+        }
+    }
+}
